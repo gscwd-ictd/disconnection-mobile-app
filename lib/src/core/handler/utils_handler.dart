5bea31ca7a04c34e39fc9eec832b257ebe6f0c9d
iansrnf <ianspencer0904@gmail.com>
2024-01-30 08:16:22 +0800

    with custom link for api handler

--- lib/src/core/handler/utils_handler.dart
+++ lib/src/core/handler/utils_handler.dart
@@ -1,8 +1,9 @@
 import 'package:diconnection/src/data/models/zone_model.dart';
 import 'package:image_picker/image_picker.dart';
 
-class UtilsHandler{
+class UtilsHandler {
   static bool hasConnection = true;
   static List<ZoneModel> zones = [];
   static List<XFile>? mediaFileList = [];
+  static String apiLink = "";
 }
